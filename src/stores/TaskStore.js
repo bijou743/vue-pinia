4886b3a6f01da50c4e0acff2562b274366ae2b2e
Maria Sorokina <113168296+bijou743@users.noreply.github.com>
2022-10-12 13:34:37 +0300

    first commit

---
+++ src/stores/TaskStore.js
@@ -0,0 +1,37 @@
+import { defineStore } from 'pinia';
+
+export const useTaskStore = defineStore('taskStore', {
+	state: () => ({
+		tasks: [
+			{ id: 1, title: 'buy some milk', isFav: false },
+			{ id: 2, title: 'play Gloomhaven', isFav: true },
+		],
+	}),
+	getters: {
+		favs() {
+			return this.tasks.filter((item) => item.isFav);
+		},
+		favCount() {
+			return this.tasks.reduce((p, c) => {
+				return c.isFav ? p + 1 : p;
+			}, 0);
+		},
+		totalCount: (state) => {
+			return state.tasks.length;
+		},
+	},
+	actions: {
+		addTask(task) {
+			this.tasks.push(task);
+		},
+		deleteTask(id) {
+			this.tasks = this.tasks.filter((task) => {
+				return task.id !== id;
+			});
+		},
+		toggleFav(id) {
+			const task = this.tasks.find((task) => task.id === id);
+			task.isFav = !task.isFav;
+		},
+	},
+});
